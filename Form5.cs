f5401abcdd1ad58821c0c323892143aa7933b875
Austin-GitHub <84061326+Austin2gitgub@users.noreply.github.com>
2021-11-25 14:44:36 +0800

    Added Report Mail feature
    Added the mail feature to the Report Form to our Email

--- Form5.cs
+++ Form5.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
 
 namespace AhyokoPBLReTech
 {
@@ -29,6 +31,31 @@ namespace AhyokoPBLReTech
 
         private void button2_Click(object sender, EventArgs e)
         {
+            try
+            {
+                MailMessage msg = new MailMessage();
+                msg.From = new MailAddress("ahyoko.29@gmail.com");
+                msg.To.Add("ahyoko.29@gmail.com");
+                msg.Subject = "Student Harrassment Report" + "Reported at: " + DateTime.Now.ToString();
+                msg.Body = textBox1.Text;
+
+                SmtpClient smt = new SmtpClient();
+                smt.Host = "smtp.gmail.com";
+                System.Net.NetworkCredential ntcd = new NetworkCredential();
+                ntcd.UserName = "ahyoko.29@gmail.com";
+                ntcd.Password = "Pbl2021.";
+                smt.Credentials = ntcd;
+                smt.EnableSsl = true;
+                smt.Port = 587;
+                smt.Send(msg);
+
+                MessageBox.Show("Your Report has been sent");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
             this.Hide();
             Form6 form6 = new Form6();
             form6.ShowDialog();
